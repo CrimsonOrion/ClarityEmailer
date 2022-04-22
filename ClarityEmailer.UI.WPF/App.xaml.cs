aab0a01d125af4c7d710e7b783a5cebbdf48cac9
James Lynch <crimsonorion@gmail.com>
2022-04-22 12:27:34 -0500

    Added WPF UI

---
+++ ClarityEmailer.UI.WPF/App.xaml.cs
@@ -0,0 +1,66 @@
+﻿using ClarityEmailer.Core;
+using ClarityEmailer.Core.Processors;
+
+using ControlzEx.Theming;
+
+using Library.NET.Logging;
+
+using MahApps.Metro.Controls.Dialogs;
+
+using Microsoft.Extensions.Configuration;
+
+using Prism.Ioc;
+
+using System;
+using System.Windows;
+
+namespace ClarityEmailer.UI.WPF;
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    protected override Window CreateShell() => Container.Resolve<MainWindow>();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        ThemeManager.Current.ThemeSyncMode = ThemeSyncMode.SyncAll;
+        ThemeManager.Current.SyncTheme();
+    }
+
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        // Set up the custom logger to go to Desktop
+        ICustomLogger logger = new CustomLogger(new("WPFLog.txt"), true, LogLevel.Information);
+
+        // Set up the configuration using the appSettings.json file.
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("appSettings.json", false, true)
+            .Build();
+
+
+        GlobalConfig.EmailConfig = new()
+        {
+            SmtpServer = configuration["Email Settings:Smtp Server"] ?? "",
+            SmtpPort = Convert.ToInt16((configuration["Email Settings:Smtp Port"]) ?? "25"),
+            SenderEmail = configuration["Email Settings:Sender Email"] ?? "",
+            Password = configuration["Email Settings:Password"] ?? ""
+        };
+
+        containerRegistry
+            .RegisterInstance(logger)
+
+            .RegisterInstance<IDialogCoordinator>(new DialogCoordinator())
+
+            .Register<IEmailProcessor, EmailProcessor>()
+            ;
+    }
+
+    //protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog) => moduleCatalog
+    //        .AddModule<CustomerServiceModule>()
+    //        .AddModule<MarketingModule>()
+    //        .AddModule<SupportServicesModule>()
+    //        .AddModule<AccountingModule>()
+    //        .AddModule<AboutModule>();
+}
---
+++ ClarityEmailer.UI.WPF/MainWindowViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace ClarityEmailer.UI.WPF;
+public class MainWindowViewModel : BindableBase
+{
+    private readonly IRegionManager _regionManager;
+
+    #region Main Window Properties
+
+    public static string Title => $"Clarity Emailer v{System.Reflection.Assembly.GetExecutingAssembly().GetName().Version}";
+
+    #endregion
+
+    #region Delegate Commands
+
+    #region Navigation
+
+    #region Emailer
+
+    public DelegateCommand EmailerScreenCommand => new(EmailerScreen);
+
+    private void EmailerScreen() => Navigate("EmailerView");
+
+    #endregion
+
+    #region About
+
+    public DelegateCommand AboutScreenCommand => new(AboutScreen);
+
+    private void AboutScreen() => Navigate("AboutView");
+
+    #endregion
+
+    #endregion
+
+    #endregion
+
+    #region Constructor
+
+    public MainWindowViewModel(IRegionManager regionManager) => _regionManager = regionManager;
+
+    #endregion
+
+    #region Private Methods
+
+    private void Navigate(string navigationPath, NavigationParameters navigationParameters = null) => _regionManager.RequestNavigate("MainRegion", navigationPath, navigationParameters);
+
+    #endregion
+}
