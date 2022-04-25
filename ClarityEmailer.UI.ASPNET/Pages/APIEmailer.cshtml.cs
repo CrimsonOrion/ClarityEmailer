3951620f12539cc1661288b0220c38c08ba2fee8
James Lynch <eimi_christian@hotmail.com>
2022-04-24 20:02:12 -0400

    Created ASP.NET UI

---
+++ ClarityEmailer.UI.ASPNET/Pages/APIEmailer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClarityEmailer.UI.ASPNET.Pages
+{
+    public class APIEmailerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ClarityEmailer.UI.ASPNET/Pages/LibraryEmailer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClarityEmailer.UI.ASPNET.Pages
+{
+    public class LibraryEmailerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
